a6670346e943822133e64c362b50c7b601e08b3d
Rafael <rafagondeveloper@gmail.com>
2020-03-24 12:53:30 -0500

    Avances estilos login

--- src/pages/auth/Login.vue
+++ src/pages/auth/Login.vue
@@ -1,52 +1,58 @@
 <template>
-  <q-page padding>
-    <h5 class="text-center">Ingresar </h5>
-    <q-form class="authentication q-gutter-y-md" ref="emailAuthenticationForm" @submit="onSubmit">
-      <q-input
-        v-model="email"
-        outlined
-        autocomplete="email"
-        color="primary"
-        data-cy="email"
-        for="email"
-        lazy-rules="lazy-rules"
-        name="email"
-        label="EMAIL"
-        type="email"
-        :rules="[val => !!val || '*Field is required', val => val.includes('@') && val.includes('.') || '*Please Provide a valid email']"
-      />
-      <q-input
-        v-model="password"
-        for="password"
-        name="password"
-        lazy-rules="lazy-rules"
-        outlined
-        autocomplete="current-password"
-        color="primary"
-        data-cy="password"
-        label="PASSWORD"
-        :rules="[val =&gt; !!val || '*Field is required']"
-        :type="isPwd ? 'password' : 'text'"
-        @keyup.enter="onSubmit()"
-      >
-        <template v-slot:append>
-          <q-icon class="cursor-pointer" :name="isPwd ? 'visibility_off' : 'visibility'" @click="isPwd = !isPwd" />
-        </template>
-      </q-input>
-      <q-btn
-        class="full-width q-mt-md"
-        color="primary"
-        data-cy="submit"
-        type="submit"
-        label="Ingresar"
-      >
-      </q-btn>
+    <div class="q-pa-md row items-start q-gutter-md">
+       <q-card bordered class="my-card shadow-5">
+          <q-card-section>
+              <h5 class="text-center">Ingresar</h5>
+          </q-card-section>
+          <q-card-section>
+            <q-form class="authentication q-gutter-y-md" ref="emailAuthenticationForm" @submit="onSubmit">
+              <q-input
+                v-model="email"
+                outlined
+                autocomplete="email"
+                color="primary"
+                data-cy="email"
+                for="email"
+                lazy-rules="lazy-rules"
+                name="email"
+                label="EMAIL"
+                type="email"
+                :rules="[val => !!val || '*Field is required', val => val.includes('@') && val.includes('.') || '*Please Provide a valid email']"
+              />
+              <q-input
+                v-model="password"
+                for="password"
+                name="password"
+                lazy-rules="lazy-rules"
+                outlined
+                autocomplete="current-password"
+                color="primary"
+                data-cy="password"
+                label="PASSWORD"
+                :rules="[val =&gt; !!val || '*Field is required']"
+                :type="isPwd ? 'password' : 'text'"
+                @keyup.enter="onSubmit()"
+              >
+              <template v-slot:append>
+                <q-icon class="cursor-pointer" :name="isPwd ? 'visibility_off' : 'visibility'" @click="isPwd = !isPwd" />
+              </template>
+              </q-input>
+              <q-btn
+                class="full-width q-mt-md"
+                color="primary"
+                data-cy="submit"
+                type="submit"
+                label="Ingresar"
+              >
+              </q-btn>
 
-      <p class="q-ma-sm text-center">
-        <router-link class="text-blue" to="forgotPassword">Forgot Password?</router-link>
-      </p>
-    </q-form>
-  </q-page>
+              <p class="q-ma-sm text-center">
+                <router-link class="text-blue" to="forgotPassword">Forgot Password?</router-link>
+              </p>
+            </q-form>
+          </q-card-section>
+      </q-card>
+    </div>
 </template>
 
 <script>
